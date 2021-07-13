6ae4bffe7bffd2933edd290905077c9a3a1065c1
Kabe KUŞ <kabekus.123@gmail.com>
2021-07-13 15:02:19 +0300

    User Form Screen

--- app.js
+++ app.js
@@ -1,50 +1,32 @@
-let firstName= prompt("Enter the you First Name");
+let userForm = document.querySelector("#userForm")
+userForm.addEventListener('submit', formUser);
 
-let nameOutput= document.querySelector("span#myName");
-nameOutput.innerHTML=firstName;
+const alert=document.querySelector("#alert")
 
+const alertFunction= (title, messages,color='warning') => `<div class="alert alert-${color} alert-dismissible fade show" role="alert">
+<strong> ${title} </strong> ${messages}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>` 
 
-let dayName;
-let h = "";
-let m = "";
-let s = "";
-function now(){
-
-let liveDate = new Date();
-let today = liveDate.getDay();
-let hour =liveDate.getHours();
-let minute = liveDate.getMinutes();
-let second = liveDate.getSeconds();
-
-if(today == 0){
-    dayName = "Sunday"
-}else if(today == 1){
-	dayName = "Monday";
-}else if(today == 2){
-	dayName = "Tuesday";
-}else if(today == 3){
-	dayName = "Wedneday";
-}else if(today == 4){
-	dayName = "Thursday";
-}else if(today == 5){
-	dayName = "Friday";
-}else if(today == 6){
-	dayName = "Saturday";
- }
-
-if(hour<10){
-	h="0";
-}
-if(minute<10){
-	m="0";
-}
-if(second<10){
-	s="0";
-}
- 
-let divDom =document.querySelector("div#myClock");
-divDom.innerHTML = `${h}${hour}:${m}${minute}:${s}${second}  ${dayName}`;
- 
+function formUser(event){
+	event.preventDefault();
+	const userName=document.querySelector("#username")
+	const score = document.querySelector("#score")
+	if(userName.value, score.value ){
+	   adLi(userName.value, score.value )
+	   userName.value="",
+	   score.value=""
+	   alert.innerHTML=alertFunction("Başarılı!" , "Bilgiler Doğru" , "primary")
+	}else{
+       alert.innerHTML=alertFunction("Hata!" , "Eksik Bilgi Girdiniz!" , "danger");
+	}
 }
 
-window.setInterval("now()",1000);
+let userList = document.querySelector("#userList")
+
+function adLi(username, score){
+    let li = document.createElement('li')
+	li.innerHTML= `${username} <span class="badge bg-primary rounded-pill"> ${score} </span>`;
+	li.classList.add('list-group-item' , 'd-flex',  'justify-content-between', 'align-items-center')
+	userList.append(li);
+}
