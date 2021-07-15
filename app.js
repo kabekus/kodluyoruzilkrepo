7ca89a0ff4e0329073303e8fbad682e899800335
Kabe KUŞ <kabekus.123@gmail.com>
2021-07-15 13:58:46 +0300

    .mat Metodu

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const urunler = ["Mikrofon","Kablo","Telefon","Bilgisayar","Mouse","Klavye","Ekran"]
-const filtreliUrun = urunler.filter(eleman => eleman.length > 3)
-//filter metodu ile dizi elemanlarını döngüye alırız. istediğimiz koşula sahip olanlarla yei dizi oluştururuz.
-console.log(filtreliUrun);
-//--------------------
+const users = ["Ayşe","MehMet","TuğCE"]
+const userObj=users.map(item=>(
+    {userName:item,shortName:`${item[0]}.`,newName:`${item[0].toUpperCase()}${item.slice(1).toLowerCase()}`}
+  
+  )
+  )
+  console.log(userObj);
 
-const person = [{name: "Adem",age: 25,languages: ["JavaScript", "CSS"]},
-                {name: "Oğuz",age: 33,languages: ["Java", "HTML"]},
-                {name: "Ayşe",age: 35,languages: ["C#", "JavaScript"]},
-                {name: "Melike",age:20,languages: ["Java", "JavaScript"]}
-            ];
- const filterAge= person.filter(yas=>yas.age>30)
- console.log(filterAge);
- const filterLanguage=person.filter(dil=>dil.languages.includes("JavaScript"))
- console.log(filterLanguage);
+//Array Map metodu:
+//parametre ile kendisine gönderilen dizinin her bir elemanı için ayrı ayrı çalışır ve belirlenen işleme tabi tutup, yeni bir dizi meydana getirir.
